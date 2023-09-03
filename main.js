56eb43fb39baca24d917999938aa8846ba8b4c24
rexx0n <alexey.mironenko46@gmail.com>
2023-09-03 17:33:01 +0300

    made burger

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 import './scss/style.scss'
+let burger = document.querySelector('.header__burger')
+let svgBurger = document.querySelector('.header__burger > svg')
+let navBurger = document.querySelector('.header__nav')
+let navCross = document.querySelector('.header__nav-cross')
+svgBurger.addEventListener('click', ()=> {
+    svgBurger.classList.add('header__none')
+    navBurger.classList.remove('header__nav--none')
+})
+navCross.addEventListener('click', ()=> {
+    svgBurger.classList.remove('header__none')
+    navBurger.classList.add('header__nav--none')
+})
